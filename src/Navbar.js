6fe81a9376f69a1d9a96f5bb9c01c0fec7d3bf3b
MdSheezanQumar <sheezanqumar10@gmail.com>
2023-12-18 12:57:24 +0530

    v

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,33 +1,35 @@
 
 import { Link } from "react-router-dom";
-
+import citytour from './citytour.jpeg'
 function Navbar(){
 
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-        <a className="navbar-brand" href="#"><img className="imgg px-2"src='https://tse4.mm.bing.net/th?id=OIP.Yx0uJruCrTz85C30Mvzt4wHaGp&pid=Api&P=0&h=80'/></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" style={{color:'yellowgreen'}} href="#" to="/">Home </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link"  style={{color:'yellowgreen'}}  href="#" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link"  style={{color:'yellowgreen'}}  href="#" to="/holiday">Holidays</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link"  style={{color:'yellowgreen'}} href="#">Details</Link>
-            </li>
-            
-          </ul>
-        </div>
-      </nav>
+      <div className='container-fluid'>
+      <div className='row ' >
+          <div className='d-flex nav1 ' style={{backgroundColor: '#e3f2fd'}}>
+           
+              <img src={citytour} className='img-fluid my-1' style={{height:'80px'}}></img>
+                    
+              <ul className="nav my-3">
+              
+                  
+                     <li className="nav-item">
+                    <Link className="nav-link " to='/'>Home</Link>
+                     </li>
+                     <li className="nav-item">
+                     <Link className="nav-link" to="/about">About</Link>
+                     </li>
+                     <li className="nav-item">
+                     <Link className="nav-link" to='/holiday'>Holidays</Link>
+                     </li>
+                     <li className="nav-item">
+                     <Link className="nav-link" href="#">Details</Link>
+                     </li>
+                     
+              </ul>
+          </div>
+      </div>
+  </div>
     );
 
 }
